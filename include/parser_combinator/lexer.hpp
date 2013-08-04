8e7a4d0a3de4919a95b40738cce0c4a84b2ae3da
南山まさかず <masakazu.minamiyama@gmail.com>
2013-08-05 02:07:07 +0900

    Lexical analyzer complete.

--- include/parser_combinator/lexer.hpp
+++ include/parser_combinator/lexer.hpp
@@ -1,10 +1,11 @@
 #ifndef PARSER_COMBINATOR_LEXER_HPP
 #define PARSER_COMBINATOR_LEXER_HPP
+#include<iostream>
 #include<string>
-#include<unordered_map>
 #include"boost/xpressive/basic_regex.hpp"
 #include"boost/xpressive/regex_algorithms.hpp"
 #include"boost/xpressive/regex_primitives.hpp"
+#include"boost/xpressive/regex_actions.hpp"
 namespace parser_combinator
 {
 	namespace lexer
@@ -14,18 +15,19 @@ namespace parser_combinator
 		{
 			using regex_type = boost::xpressive::basic_regex < iterator_type > ;
 			using match_results_type = boost::xpressive::match_results < iterator_type > ;
+			using regex_id_filter_predicate_type = boost::xpressive::regex_id_filter_predicate < iterator_type > ;
 			using string_type = std::basic_string < typename std::iterator_traits < iterator_type >::value_type > ;
-			using action_type = std::function < std::string ( const string_type & ) > ;
+			using action_type = std::function < void ( const string_type & ) > ;
+			using regex_id_type = decltype ( regex_type { }.regex_id ( ) ) ;
 			iterator_type begin_ ;
 			iterator_type end_ ;
-			regex_type regex_ { boost::xpressive::_ } ;
-			std::unordered_map < decltype ( regex_type { }.regex_id ( ) ) , action_type > action_list_ ;
+			regex_type regex_ ;
 		public :
 			lexer ( ) = delete ;
 			lexer ( const lexer & ) = delete ;
-			lexer ( lexer && ) = delete ;
+			lexer ( lexer && ) = default ;
 			auto operator = ( const lexer & ) -> lexer & = delete ;
-			auto operator = ( lexer && ) -> lexer & = delete ;
+			auto operator = ( lexer && ) -> lexer & = default ;
 			~ lexer ( ) = default ;
 			lexer ( iterator_type begin , iterator_type end ) ;
 			auto operator ( ) ( const regex_type & regex , const action_type & action ) -> void ;
@@ -40,18 +42,29 @@ namespace parser_combinator
 		template < typename iterator_type >
 		auto lexer < iterator_type >::operator ( ) ( const regex_type & regex , const action_type & action ) -> void
 		{
-			regex_ = ( boost::xpressive::bos >> regex ) | regex_ ;
-			action_list_.emplace ( regex.regex_id ( ) , action ) ;
+			regex_type new_regex { boost::xpressive::bos >> regex } ;
+			if ( ! regex_.regex_id ( ) )
+			{
+				regex_ = new_regex [ boost::xpressive::ref ( action ) ( boost::xpressive::as < string_type > ( boost::xpressive::_ ) ) ] ;
+			}
+			else
+			{
+				regex_type tmp_regex { regex_ | new_regex [ boost::xpressive::ref ( action ) ( boost::xpressive::as < string_type > ( boost::xpressive::_ ) ) ] } ;
+				regex_ = tmp_regex ;
+			}
 		}
 		template < typename iterator_type >
 		auto lexer < iterator_type >::operator ( ) ( ) -> void
 		try
 		{
+			regex_type new_regex { boost::xpressive::bos >> boost::xpressive::_ } ;
+			action_type action { [ & ] ( const string_type & ) { std::cout << "Error." << std::endl ; } } ;
+			regex_type tmp_regex { regex_ | new_regex [ boost::xpressive::ref ( action ) ( boost::xpressive::as < string_type > ( boost::xpressive::_ ) ) ] } ;
+			regex_ = tmp_regex ;
 			match_results_type result ;
 			for ( ; boost::xpressive::regex_search ( begin_ , end_ , result , regex_ ) && ( begin_ != end_ ) ; )
 			{
-				action_list_.at ( result.regex_id ( ) ) ( result.str ( ) ) ;
-				begin_ = result.suffix ( ).first ;
+				begin_ = result [ 0 ].second ;
 			}
 			if ( begin_ != end_ )
 			{
@@ -63,7 +76,11 @@ namespace parser_combinator
 			begin_ = end_ ;
 			throw ;
 		}
-		template class lexer < std::string::const_iterator > ;
+		template < typename iterator_type >
+		auto make_lexer ( iterator_type begin , iterator_type end ) -> lexer < iterator_type >
+		{
+			return lexer < iterator_type > { begin , end } ;
+		}
 	}
 }
 #endif
